4df21ecdd87cb58c68b00187852b2fe3c4861943
rasoolabdi <M0rd00r@yahoo.com>
2024-07-25 10:43:07 +0330

    edit producer

--- producer.js
+++ producer.js
@@ -1,17 +1,20 @@
 const amqp = require("amqplib");
 const exchangeName = "HeaderMessage";
-const [logTypes , message] = process.argv.slice(2);
-
 
 async function sendData() {
     const connection = await amqp.connect("amqp://localhost:5672");
     const channel = await connection.createChannel();
     await channel.assertExchange(exchangeName , "headers");
-    channel.publish(exchangeName , logTypes , Buffer.from(message));
+    channel.publish(exchangeName , "" ,Buffer.from(message) , {headers: {
+        author: "rasoolabdi",
+        runtime: "nodejs",
+        price: 1200,
+        comments: []
+    }});
     setTimeout(() => {
         connection.close();
         process.exit(0);
     })
 }
 
-sendData();
+sendData();             
